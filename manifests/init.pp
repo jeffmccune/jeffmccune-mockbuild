674a108ed3e5161fb3f0829be4369cb5344510b9
Jeff McCune <jeff@puppetlabs.com>
2011-03-17 12:00:52 -0700

    Initial commit.
    This module will configure a build environment for Enterprise Linux
    systems.  It will be primarily developed on a vagrant system.

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: mockbuild
+#
+# This module manages mockbuild
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class mockbuild {
+
+
+}
